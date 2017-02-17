057dd4a7dbb4eb42dfc52c9935392e1d9be8ee99
Spefire <nicholasb@hotmail.fr>
2017-02-17 16:58:24 +0100

    fix(choices): fix the selection of choices and add sound

--- Assets/Scripts/Menu/Choice_J1.cs
+++ Assets/Scripts/Menu/Choice_J1.cs
@@ -9,13 +9,16 @@ public class Choice_J1 : MonoBehaviour {
 	public Texture texture_choice_0;
 	public Texture texture_choice_1;
 	public Texture texture_choice_2;
+	public AudioClip son_selection;
 	private Renderer render;
+	private AudioSource aud;
 
 
 	void Start () {
 		pos = 1;
 		choice = 0;
 		render = GetComponent<Renderer>();
+		aud = GetComponent<AudioSource>();
 	}
 
 
@@ -28,14 +31,17 @@ public class Choice_J1 : MonoBehaviour {
 			else if ((choice == 1 || choice == 2) && Input.GetKeyUp (KeyCode.Z)) {
 				choice = 0;
 				render.material.mainTexture = texture_choice_0;
+				aud.PlayOneShot (son_selection);
 			}
 			else if (choice == 1 && Input.GetKeyUp (KeyCode.D)) {
 				choice = 2;
 				render.material.mainTexture = texture_choice_2;
+				aud.PlayOneShot (son_selection);
 			}
 			else if (choice == 2 && Input.GetKeyUp (KeyCode.Q)) {
 				choice = 1;
 				render.material.mainTexture = texture_choice_1;
+				aud.PlayOneShot (son_selection);
 			}
 		}
 	}
--- Assets/Scripts/Menu/Choice_Menu.cs
+++ Assets/Scripts/Menu/Choice_Menu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Choice_Menu : MonoBehaviour {
 
+	private int pos;
 	public static int choice;
 	public Texture texture_choice_0;
 	public Texture texture_choice_1;
@@ -11,54 +12,62 @@ public class Choice_Menu : MonoBehaviour {
 	public Texture texture_choice_3;
 	public Texture texture_choice_4;
 	public Texture texture_choice_5;
+	public AudioClip son_selection;
 	private Renderer render;
+	private AudioSource aud;
 
 
 	void Start () {
+		pos = 0;
 		choice = 0;
 		render = GetComponent<Renderer>();
+		aud = GetComponent<AudioSource>();
 	}
 
 
 	void Update () {
-		if (choice < 5 && Input.GetKeyUp (KeyCode.S)) {
-			choice++;
-			switch (choice) {
-			case 1: //Aides
-				render.material.mainTexture = texture_choice_1;
-				break;
-			case 2: //Credits
-				render.material.mainTexture = texture_choice_2;
-				break;
-			case 3: //Musique
-				render.material.mainTexture = texture_choice_3;
-				break;
-			case 4: //Langage
-				render.material.mainTexture = texture_choice_4;
-				break;
-			case 5: //Quitter
-				render.material.mainTexture = texture_choice_5;
-				break;
+		if (Camera_Menu.pos == pos) {
+			if (choice < 5 && Input.GetKeyUp (KeyCode.S)) {
+				choice++;
+				aud.PlayOneShot (son_selection);
+				switch (choice) {
+				case 1: //Aides
+					render.material.mainTexture = texture_choice_1;
+					break;
+				case 2: //Credits
+					render.material.mainTexture = texture_choice_2;
+					break;
+				case 3: //Musique
+					render.material.mainTexture = texture_choice_3;
+					break;
+				case 4: //Langage
+					render.material.mainTexture = texture_choice_4;
+					break;
+				case 5: //Quitter
+					render.material.mainTexture = texture_choice_5;
+					break;
+				}
 			}
-		}
-		if (choice > 0 && Input.GetKeyUp (KeyCode.Z)) {
-			choice--;
-			switch (choice) {
-			case 0: //Jouer
-				render.material.mainTexture = texture_choice_0;
-				break;
-			case 1: //Aides
-				render.material.mainTexture = texture_choice_1;
-				break;
-			case 2: //Credits
-				render.material.mainTexture = texture_choice_2;
-				break;
-			case 3: //Musique
-				render.material.mainTexture = texture_choice_3;
-				break;
-			case 4: //Langage
-				render.material.mainTexture = texture_choice_4;
-				break;
+			if (choice > 0 && Input.GetKeyUp (KeyCode.Z)) {
+				choice--;
+				aud.PlayOneShot (son_selection);
+				switch (choice) {
+				case 0: //Jouer
+					render.material.mainTexture = texture_choice_0;
+					break;
+				case 1: //Aides
+					render.material.mainTexture = texture_choice_1;
+					break;
+				case 2: //Credits
+					render.material.mainTexture = texture_choice_2;
+					break;
+				case 3: //Musique
+					render.material.mainTexture = texture_choice_3;
+					break;
+				case 4: //Langage
+					render.material.mainTexture = texture_choice_4;
+					break;
+				}
 			}
 		}
 	}
