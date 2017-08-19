dfc27e96f1f319324d6aef0402851d402f6c5471
Spefire <nicholasb@hotmail.fr>
2017-08-19 16:35:29 +0200

    fix(menu): fix selection sound

--- Assets/NewsAssets/Scripts/Menu/Selection_Aides_RK.cs
+++ Assets/NewsAssets/Scripts/Menu/Selection_Aides_RK.cs
@@ -2,24 +2,24 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class Selection_Menu_RK : MonoBehaviour {
+public class Selection_Aides_RK : MonoBehaviour {
 
-	public int select_menu;
+	public int select_aide;
 	public AudioClip son_selection;
 	private AudioSource aud;
 
 	void OnEnable () {
-		select_menu = 1;
+		select_aide = 2;
 		aud = GetComponent<AudioSource>();
 	}
 
 	void Update () {
-		if (Game_Inputs.J1_Bas && select_menu < 5) {
-			select_menu++;
+		if (Game_Inputs.J1_Droit && select_aide < 2) {
+			select_aide++;
 			Affichage ();
 		}
-		else if (Game_Inputs.J1_Haut && select_menu > 1) {
-			select_menu--;
+		else if (Game_Inputs.J1_Gauche && select_aide > 1) {
+			select_aide--;
 			Affichage ();
 		}
 	}
