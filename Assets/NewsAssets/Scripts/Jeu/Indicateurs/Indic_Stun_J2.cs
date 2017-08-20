6341b90981426f8e4babda79f6f6c79607e53c2e
Spefire <nicholasb@hotmail.fr>
2017-08-20 15:16:11 +0200

    refractor(code): prepare indicators

---
+++ Assets/NewsAssets/Scripts/Jeu/Indicateurs/Indic_Stun_J2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Indic_Stun_J2 : MonoBehaviour {
+
+	public Texture texture_base;
+	public Texture texture_stun;
+	private static bool stunned;
+	private Renderer render;
+
+	void Start () {
+		render = GetComponent<Renderer>();
+		stunned = false;
+	}
+
+	void Update () {
+		/*if (Joueur_Deplacement_J2.stunned && !stunned) {
+			stunned = true;
+			render.material.mainTexture = texture_stun;
+		}
+		else if (!Joueur_Deplacement_J2.stunned && stunned) {
+			stunned = false;
+			render.material.mainTexture = texture_base;
+		}*/
+	}
+}
+
---
+++ Assets/NewsAssets/Scripts/Jeu/Objets/Objet_Fouet.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objet_Fouet : MonoBehaviour {
+
+	public double coeffDegats;
+	public int resistance;
+
+	private GameObject ally;
+	private GameObject enemy;
+	private bool inversed;
+	private SpriteRenderer render;
+
+	void OnEnable () {
+		render = GetComponent<SpriteRenderer>();
+	}
+
+	void Update () {
+		this.transform.position = ally.transform.position;
+	}
+
+	public void SetConfig(bool first, bool inversed) {
+		if (first) {
+			ally = GameObject.FindGameObjectWithTag ("J1");
+			enemy = GameObject.FindGameObjectWithTag ("J2");
+		} else {
+			ally = GameObject.FindGameObjectWithTag ("J2");
+			enemy = GameObject.FindGameObjectWithTag ("J1");
+		}
+		if (inversed) {
+			render.flipX = true;
+		}
+		Physics.IgnoreCollision(this.GetComponent<Collider>(), ally.transform.root.GetComponent<Collider>());
+	}
+
+	void OnTriggerEnter(Collider objetInfo) {
+		if (objetInfo.gameObject == enemy) {
+			Perso_Stats_RK statsEnemy = enemy.GetComponent<Perso_Stats_RK> ();
+			statsEnemy.SetDamage (statsEnemy.GetDamage (coeffDegats), resistance);
+			if (enemy.transform.position.x > transform.position.x) {
+				enemy.GetComponent<Rigidbody>().AddForce (Vector3.up * 120, ForceMode.Impulse);
+				enemy.GetComponent<Rigidbody>().AddForce (Vector3.right * 30, ForceMode.Impulse);
+			} else {
+				enemy.GetComponent<Rigidbody>().AddForce (Vector3.up * 120, ForceMode.Impulse);
+				enemy.GetComponent<Rigidbody>().AddForce (Vector3.left * 30, ForceMode.Impulse);
+			}
+			DestroyObject (this.gameObject);
+		}
+	}
+
+}
